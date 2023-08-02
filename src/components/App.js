14480c659bbc365d9e98c37fe64d9d622fe11a74
Rakibul Islam <103498045+Rakib7425@users.noreply.github.com>
2023-08-02 15:22:50 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/App.css';
 import { Loader } from './Loader';
 import { PhotoFrame } from './PhotoFrame';
 const App = () => {
-  
+  const [id, setId] = useState()
+  const [loading, setLoading] = useState(false)
+  const [imgData, setImgData] = useState()
+  const handleNumberChange = (e) => {
+    const number = e.target.value
+    async function fetchData(id) {
+      try {
+        setLoading(true)
+        const rawData = await fetch(`https://jsonplaceholder.typicode.com/photos/${id}`)
+        const data = await rawData.json()
+        setImgData(data)
+      } catch (error) {
+
+      } finally {
+        setLoading(false)
+      }
+    }
+    setId(number)
+    fetchData(number)
+  }
+  return (
+    <div id="main">
+      Id number&nbsp;
+      <input type={"number"} value={id} onChange={handleNumberChange} />
+      {
+        loading ?
+          <Loader />
+          :
+          !loading && imgData && id !== 0 ? <PhotoFrame id={imgData.id} url={imgData.url} title={imgData.title} /> : null
+      }
+    </div>
+  )
 }
 
 
