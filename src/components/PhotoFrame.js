3b7332821080abcc4054da5c9eb200bf6462f048
Rakibul Islam <103498045+Rakib7425@users.noreply.github.com>
2023-08-02 15:23:31 +0530

    Update PhotoFrame.js

--- src/components/PhotoFrame.js
+++ src/components/PhotoFrame.js
@@ -1,5 +1,9 @@
 import React from 'react';
-
-export const PhotoFrame = () => {
-   
+export const PhotoFrame = ({id,title,url}) => {
+    return(
+        <div className='photoframe' id={`photoframe-${id}`}>
+            <img src={url}/>
+            <div className='caption'>{title}</div>
+        </div>
+    )
 }
